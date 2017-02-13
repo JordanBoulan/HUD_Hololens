bf4abdc03c06265d4054cb97cfa92aa92a129161
kdo2585 <kevindo0511@gmail.com>
2017-02-12 18:48:57 -0800

    integrating allens textures

---
+++ Assets/Scripts/Battery1.cs
@@ -0,0 +1,54 @@
+﻿/* 
+ * @author: Kevin Do
+ * @discription: create image graphic, and change the size of the width to show battery decreasing
+ *  
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Battery1 : MonoBehaviour {
+
+    public static int xVal=100;//value of the width of the image
+    public Color color;//color of the image
+    public static int count = 0;
+    private UnityEngine.UI.RawImage battery;
+    
+
+    // Use this for initialization
+    void Start () {
+        battery = GetComponent<RawImage>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(xVal, 28);//create image size 100x100
+        count++;
+        if (count == 20)
+        {
+            xVal = xVal - 5; //decrement the width of image
+            count = 0;
+            //reset battery to 100 when it gets to 0 (value doesnt go negative)
+            if (xVal == 0)
+            {
+                xVal = 100;
+            }
+        }
+        
+        if (xVal >= 0 && xVal <= 10)
+        {
+            GetComponent<RawImage>().color = new Color32(255, 0, 0, 255); //red
+        }
+        else if (xVal > 10 && xVal <= 25)
+        {
+            GetComponent<RawImage>().color = new Color32(251, 255, 0, 255);//yellow
+        }
+        else if (xVal>25 && xVal<=100)
+        {
+            GetComponent<RawImage>().color = new Color32(33, 255, 47, 255);//green
+        }
+    }
+    
+}
---
+++ Assets/Scripts/UdpEvent.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+/*
+Author: Jordan Boulanger
+An event to notifiy the other Unity scripts that data has been recieved
+*/
+
+public class UdpEvent : MonoBehaviour
+{
+
+    public delegate void DataRecievedEvent(DataStruct newData); // event type
+
+    public static event DataRecievedEvent dataRecieved;  //event instance
+ 
+    public static void onDataRecieved(DataStruct myNewData)  // function to fire event
+    {
+        if (dataRecieved != null)
+        {
+
+            dataRecieved(myNewData);
+        }
+    }
+
+	
+	/*
+	in other unity script register a local function to be called when app recieves data:
+	
+	void start(){
+		udpEvent.dataRecieved += new DataRecievedEvent(nameofLocalFunction);
+	}
+	void nameofLocalFunction(DataStruct newData){
+		newData.altitude = myLocalalitude
+	}
+	*/
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
