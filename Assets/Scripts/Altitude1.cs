88acc3df971a1c0f698c1fd430520d09fb554849
kdo2585 <kevindo0511@gmail.com>
2017-02-15 16:54:59 -0800

    adding clipping to altitude

--- Assets/Scripts/Altitude1.cs
+++ Assets/Scripts/Altitude1.cs
@@ -4,20 +4,26 @@ using System.Collections;
 
 public class Altitude1 : MonoBehaviour
 {
-    public float maxAltitude = 10f;
-    public float altitude = 10f;
+    public static float maxAltitude = 10f;
+    public static float altitude = 10f;
     public Text AltitudeTextMoving;
-    public GameObject altitudeBar;
+    private Image altitudeBar;
+
 
     // Use this for initialization
     void Start()
     {
+        altitudeBar = GameObject.Find("AltitudeBar").GetComponent<Image>();
+        altitudeBar.fillAmount = 0f;
+        
+        AltitudeTextMoving = GameObject.Find("AltitudeTextMoving").GetComponent<Text>();
         AltitudeTextMoving.text = altitude.ToString();
         InvokeRepeating("ReduceAltitude", 0f, 0.5f);
     }
 
     void ReduceAltitude()
     {
+        altitudeBar.fillAmount = altitude / 100;
         altitude -= 1f;
         if (altitude < 0f)
         {
@@ -30,22 +36,13 @@ public class Altitude1 : MonoBehaviour
     public void SetAltitudeBar(float myAltitude)
     {
         //myAltitude is value 0, 1
-        float calc_altitude = myAltitude / maxAltitude;
-        if (calc_altitude == 0)
-        {
-            altitudeBar.transform.localScale = new Vector3(1f, 0.001f, 1f);
-            //AltitudeTextMoving.transform.localScale = new Vector3(1f, 1000f, 1f);
-        }
-        else
-        {
-            altitudeBar.transform.localScale = new Vector3(1f, calc_altitude, 1f);
-           // AltitudeTextMoving.transform.localScale = new Vector3(1f, (1 / calc_altitude), 1f);
-        }
+        altitudeBar.fillAmount = myAltitude / maxAltitude;
+        
     }
-
+ 
     // Update is called once per frame
     void Update()
     {
-
+       
     }
 }
--- Assets/Scripts/Battery1.cs
+++ Assets/Scripts/Battery1.cs
@@ -8,23 +8,30 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class Battery1 : MonoBehaviour {
 
-    public static int xVal=100;//value of the width of the image
+    public static float xVal = 100f;//value of the width of the image
     public Color color;//color of the image
     public static int count = 0;
-    private UnityEngine.UI.RawImage battery;
+    private Image battery;
     
 
     // Use this for initialization
     void Start () {
-        battery = GetComponent<RawImage>();
+        battery = GetComponent<Image>();
+        battery.fillAmount = 0f;
+
+
     }
 
 	// Update is called once per frame
 	void Update ()
     {
-        GetComponent<RectTransform>().sizeDelta = new Vector2(xVal, 28);//create image size 100x100
+
+        battery.fillAmount = xVal / 100;
+
+
         count++;
         if (count == 20)
         {
@@ -39,16 +46,17 @@ public class Battery1 : MonoBehaviour {
         
         if (xVal >= 0 && xVal <= 10)
         {
-            GetComponent<RawImage>().color = new Color32(255, 0, 0, 255); //red
+            GetComponent<Image>().color = new Color32(255, 0, 0, 255); //red
         }
         else if (xVal > 10 && xVal <= 25)
         {
-            GetComponent<RawImage>().color = new Color32(251, 255, 0, 255);//yellow
+            GetComponent<Image>().color = new Color32(251, 255, 0, 255);//yellow
         }
         else if (xVal>25 && xVal<=100)
         {
-            GetComponent<RawImage>().color = new Color32(33, 255, 47, 255);//green
+            GetComponent<Image>().color = new Color32(33, 255, 47, 255);//green
         }
+        
     }
     
 }
